c055fea5d8813b9e9acbaece049925f226c9b197
heli3939 <luo325692@gmail.com>
2025-03-29 12:07:18 +1100

    layout

--- showcase-site/src/submission-widgets/MyWidget.jsx
+++ showcase-site/src/submission-widgets/MyWidget.jsx
@@ -2,27 +2,32 @@ import React, { useState } from 'react';
 
 const MyWidget = () => {
   const [text, setText] = useState('Hello, World!');
-
-  const changeText = () => setText('Text has been changed!');
+  const userName = "Name"; // corresponding level name
 
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg">
-      <div className="text-center space-y-4">
-        <h2 className="text-xl font-bold text-gray-800">Empty Component</h2>
+    <div className="p-6 max-w-4xl mx-auto bg-white rounded-xl shadow-lg flex justify-between items-start">
+      <div className="bg-white rounded-xl shadow-md  h-[500px] flex flex-col">
+        <h2 className="text-3xl font-bold text-gray-800">Hello {userName}!</h2>
+        <div className="text-xl font-bold text-blue-600">
+          Daily Tasks
 
-        <div className="text-2xl font-bold text-blue-600">
-          {text}
         </div>
-
-        <div className="flex justify-center">
-          <button
-            onClick={changeText}
-            className="p-2 bg-blue-100 rounded-full hover:bg-blue-200 transition-colors"
-          >
-            Change Text
-          </button>
+        <div className="border-4 bg-green-500 w-40 h-70 bg-clip-border p-3">To-do List
+        </div>
+        <div className="bg-white rounded-xl shadow-md p-4 flex flex-col justify-end">
+          <div className="text-xl font-bold text-indigo-600">Current Status</div>
+          <div className="border-4 bg-blue-500 w-40 h-10 bg-clip-border p-3">HP Bar</div>
+          <div className="border-4 bg-cyan-500 w-40 h-10 bg-clip-border p-3">XP Bar</div>
         </div>
       </div>
+      <div className="bg-white rounded-xl shadow-md p-4 w-1/2 h-[500px] flex flex-col justify-between ">
+        <div className="text-xl font-bold text-indigo-600">User Profile</div>
+        <div className="border-4 bg-blue-500 w-40 h-40 bg-clip-border p-3">Emoji Here</div>
+        <div className="border-4 bg-cyan-500 w-50 h-40 bg-clip-border p-3">Level Description</div>
+      </div>
+
+
+
     </div>
   );
 };
