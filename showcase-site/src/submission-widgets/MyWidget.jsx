ec449770d098e9a35e24bac023a3b65b06872ebe
borlottie <111570591+borlottie@users.noreply.github.com>
2025-03-29 11:08:37 +1100

    Added xp bar

--- showcase-site/src/submission-widgets/MyWidget.jsx
+++ showcase-site/src/submission-widgets/MyWidget.jsx
@@ -8,7 +8,7 @@ const MyWidget = () => {
   return (
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg">
       <div className="text-center space-y-4">
-        <h2 className="text-xl font-bold text-gray-800">Empty Component</h2>
+        <h2 className="text-xl font-bold text-gray-800">Our habit tracker</h2>
 
         <div className="text-2xl font-bold text-blue-600">
           {text}
@@ -23,8 +23,24 @@ const MyWidget = () => {
           </button>
         </div>
       </div>
+      <ProgressBar color="Red" />
     </div>
   );
 };
+
+const ProgressBar = (props) => {
+  const [progress, setProgress] = useState(0);
+  return (
+    <div className="size-100% bg-gray-200 rounded-full h-4 block mt-4 mb-4"> 
+    <div 
+      className="rounded-full h-10 transition-all duration-500"
+      style={{
+              width: `${progress}%`,
+              color: props.color
+            }}
+    ></div>
+    </div>
+  )
+}
 
 export default MyWidget;
